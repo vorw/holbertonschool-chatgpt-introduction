f558fbe555dc79e81451464d2a88a8f5b25501a7
vorw <sultanalsanie23@gmail.com>
2025-07-14 14:56:26 +0000

    Task 0

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -1,1 +1,12 @@
-tset
+#!/usr/bin/python3
+import sys
+
+def factorial(n):
+    result = 1
+    while n > 1:
+        result *= n
+        n -= 1  # Decrement n to avoid infinite loop
+    return result
+
+f = factorial(int(sys.argv[1]))
+print(f)
